f4f8d50a733873c7e100f63bd2d5806098a30d95
Zario3000 <nazarioortizcontreras51@gmail.com>
2024-08-31 11:17:13 -0500

    se agrega el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import Swal from 'sweetalert2'
+import phrases from './utils/phrases.json'
+import Card from './components/Card'
+
+function App() {
+  
+
+
+  return (
+    <div className='app'>
+      <div className='app-arriba'>
+        <div className='app-titulo-logo'>
+          <div className='app-logo'>
+            <img src="../public/logo-web-v1.jpeg" alt="logo" />
+          </div>
+        <h1 className='app-titulo'>MIRADA SALUDABLE</h1>
+        </div>
+        <div>
+        <h2 className='app-subtítulo'>"UNA MIRADA CLARA HACIA UN FUTURO SALUDABLE"</h2>
+        </div>
+        
+      
+      <p className='app-parrafo'>En la era digital actual, es común pasar largas horas frente a una pantalla, ya sea trabajando, estudiando o disfrutando de actividades recreativas. Sin embargo, esta exposición prolongada puede tener efectos negativos sobre nuestra salud y bienestar. Utilizar un temporizador es una herramienta sencilla pero eficaz para gestionar el tiempo frente a la pantalla y promover un equilibrio saludable</p>
+      
+      </div>
+      <div className='app-card'>
+      <Card/>
+      </div>
+      <footer className='app-abajo'>
+        <div className='app-abajo-logo' >
+          <img src="../public/logo-carrera.png" alt="" />
+        </div>
+        <div className='app-abajo-titulo'>
+          <h3 className='app-titulo-3'>UNIVERSIDAD NACIONAL MICAELA BASTIDAS DE APURÍMAC</h3>
+          <hr />
+          <br />
+          <p className='app-parrafo-footer'>EAP. INGENIERÍA INFORMÁTICA Y SISTEMAS</p>
+        </div>
+      </footer>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useRef, useState } from 'react'
+import Swal from 'sweetalert2';
+import "../styles/card.css"
+
+const Card = () => {
+    const [time, setTime] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [initialTime, setInitialTime] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
+  
+    const handleChange = (event) => {
+      setTime(Number(event.target.value));
+      setInitialTime(Number(event.target.value));
+    };
+  
+    const startTimer = () => {
+      if (isActive) return; // Evitar múltiples temporizadores
+  
+      setIsActive(true);
+      const id = setInterval(() => {
+        setTime((prevTime) => {
+          if (prevTime <= 1) {
+            clearInterval(id);
+            setIsActive(false);
+            Swal.fire({
+              title: 'Es hora de tomar un descanzo, el temporizador ha llegado a cero!',
+              icon: 'info',
+              showCancelButton: true,
+              confirmButtonText: 'Reiniciar',
+              cancelButtonText: 'Aceptar',
+              cancelButtonColor: '#d33',
+              confirmButtonColor: '#3085d6',
+            }).then((result) => {
+              if (result.isConfirmed) {
+                restartCountdown();
+              }
+            });
+            return 0;
+          }
+          return prevTime - 1;
+        });
+      }, 1000);
+      setIntervalId(id);
+    };
+  
+    const resetTimer = () => {
+      setIsActive(false);
+      setTime(0);
+      setInitialTime(0);
+      if (intervalId) {
+        clearInterval(intervalId);
+        setIntervalId(null);
+      }
+    };
+  
+    const restartCountdown = () => {
+      setTime(initialTime);
+      startTimer();
+    };
+  return (
+    <div className='card-principal'>
+      <div className='card-1'>
+      <div className='card-temporizador'>
+        <div className='card-contador'>
+        <h1>{time}</h1>
+        </div>
+        <div className='card-inicio-temp'>
+      <input
+      placeholder='digite número'
+        type="number"
+        value={initialTime === 0 ? time : initialTime}
+        onChange={handleChange}
+        disabled={isActive}
+        min="0"
+      />
+      <button className='card-iniciar' onClick={startTimer} disabled={isActive}>
+        Iniciar Temporizador
+      </button>
+      </div>
+      <div className='car-reinicio'>
+      <button className='card-reiniciar' onClick={resetTimer}>
+        Reiniciar
+      </button>
+      </div>
+      
+      
+    </div>
+      </div>
+      
+    </div>
+    
+  )
+}
+
+export default Card
